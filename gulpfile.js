478769ab833599d2d9fef3538ecf7dee42ca3433
Anel <akhmetova97@gmail.com>
2025-02-19 18:25:31 +0500

    Forms

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 const dist = "./dist/";
-// const dist = 'C:/ospanel/domains/loan'; // Ссылка на вашу папку на сервере
+/* const dist = 'C:/ospanel/domains/Loaning'; */ // Ссылка на вашу папку на сервере
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/*.html")
